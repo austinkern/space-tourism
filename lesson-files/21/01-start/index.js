88ed2a60430e5bef40ac4ea3052e9409899280ab
austinkern <ak@byu.net>
2023-08-22 17:39:48 -0700

    add mobile nav toggle with functionality

--- lesson-files/21/01-start/index.js
+++ lesson-files/21/01-start/index.js
@@ -1,1 +1,18 @@
 // javascript
+
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const nav = document.querySelector(".primary-navigation");
+
+const handleMobileNavToggle = () => {
+	const isVisible = nav.getAttribute("data-visible") === "true";
+
+	if (!isVisible) {
+		nav.setAttribute("data-visible", "true");
+		navToggle.setAttribute("aria-expanded", "true");
+	} else {
+		nav.setAttribute("data-visible", "false");
+		navToggle.setAttribute("aria-expanded", "false");
+	}
+};
+
+navToggle.addEventListener("click", () => handleMobileNavToggle());
