a772c688ad9b019ab2b2af117a027078885966b4
austinkern <ak@byu.net>
2023-09-13 01:55:24 -0700

    add picture element to crew and destination pages

--- lesson-files/21/01-start/index.js
+++ lesson-files/21/01-start/index.js
@@ -1,18 +1,24 @@
-// javascript
+// navigation
 
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const nav = document.querySelector(".primary-navigation");
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
 const handleMobileNavToggle = () => {
-	const isVisible = nav.getAttribute("data-visible") === "true";
+	const isVisible = nav.getAttribute('data-visible') === 'true';
 
 	if (!isVisible) {
-		nav.setAttribute("data-visible", "true");
-		navToggle.setAttribute("aria-expanded", "true");
+		nav.setAttribute('data-visible', 'true');
+		navToggle.setAttribute('aria-expanded', 'true');
 	} else {
-		nav.setAttribute("data-visible", "false");
-		navToggle.setAttribute("aria-expanded", "false");
+		nav.setAttribute('data-visible', 'false');
+		navToggle.setAttribute('aria-expanded', 'false');
 	}
 };
 
-navToggle.addEventListener("click", () => handleMobileNavToggle());
+navToggle.addEventListener('click', () => handleMobileNavToggle());
+
+//tabs
+
+const tabs = document.querySelector('.tab-list');
+
+// console.log(tabs);
