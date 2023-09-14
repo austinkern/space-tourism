e900d396f5ae6d63eca36675bccf3c6f30a40a29
austinkern <ak@byu.net>
2023-09-14 00:39:43 -0700

    update tab panels based on tab selected

--- lesson-files/21/01-start/index.js
+++ lesson-files/21/01-start/index.js
@@ -15,10 +15,59 @@ const handleMobileNavToggle = () => {
 	}
 };
 
-navToggle.addEventListener('click', () => handleMobileNavToggle());
+navToggle.addEventListener('click', handleMobileNavToggle);
 
 //tabs
 
 const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
 
-// console.log(tabList);
+tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+	tab.addEventListener('click', changeTabPanel);
+});
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+	const keydownLeft = 37;
+	const keydownRight = 39;
+
+	// change tabindex current tab to -1 when keydownLeft or KeydownRight are pressed
+	if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+		tabs[tabFocus].setAttribute('tabindex', '-1');
+	}
+
+	// if right key pressed, move to tab on right
+	if (e.keyCode === keydownRight) {
+		if (tabFocus >= tabs.length - 1) {
+			tabFocus = 0;
+		} else {
+			tabFocus++;
+		}
+	}
+
+	// if left key pressed, move to tab on left
+	if (e.keyCode === keydownLeft) {
+		if (tabFocus <= 0) {
+			tabFocus = tabs.length - 1;
+		} else {
+			tabFocus--;
+		}
+	}
+
+	tabs[tabFocus].setAttribute('tabindex', '0');
+	tabs[tabFocus].focus();
+}
+
+function changeTabPanel(e) {
+	const targetTab = e.target;
+	const targetPanel = targetTab.getAttribute('aria-controls');
+	const tabContainer = targetTab.parentNode;
+	const mainContainer = tabContainer.parentNode;
+
+	mainContainer
+		.querySelectorAll('[role="tabpanel"]')
+		.forEach((panel) => panel.setAttribute('hidden', 'hidden'));
+	mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+}
