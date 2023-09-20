c9743c24095c7dee959cbfc0e02b2f3ab8fad1ca
austinkern <ak@byu.net>
2023-09-19 17:10:17 -0700

    add active class to primary navs, align planet img

--- lesson-files/21/01-start/index.js
+++ lesson-files/21/01-start/index.js
@@ -1,4 +1,4 @@
-// Navigation ==================================================
+// Mobile Navigation ==================================================
 
 const nav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
